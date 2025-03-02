1b811b81bfd82d7f64736c2112a1d9ffd217311f
samanpreets84 <132365157+Saman-dev12@users.noreply.github.com>
2025-03-02 21:26:58 +0530

    Sorry for that

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,33 +1,32 @@
 "use client"
 import Head from "@/components/Head";
-import {Model} from "@/components/Model";
+import { Model } from "@/components/Model";
 import { useState } from "react";
 import GenImage from "./generatedImage";
 
 export const runtime = "edge";
 
-
 export default function Main() {
-  const [image,setImage] = useState<string>('');
-  const [loading,setLoading] = useState<boolean>(false);
+  const [image, setImage] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
 
-  function generatedImage(data:string){
+  function generatedImage(data: string) { 
     setImage(data);
   }
 
-  function setLoadingState(state:boolean){
+  function setLoadingState(state: boolean) {
     setLoading(state);
   }
-  
+
   return (
     <>
       <Head />
-      <div className="flex flex-col md:flex-row items-center justify-center gap-4 h-screen p-4">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-4 min-h-screen p-4">
         <div className="flex w-full md:w-1/2 mb-4 md:mb-0">
-          <Model generatedImage={generatedImage} setLoadingState={setLoadingState}/>
+          <Model generatedImage={generatedImage} setLoadingState={setLoadingState} />
         </div>
         <div className="flex w-full md:w-1/2">
-          <GenImage image={image} loading={loading}/>
+          <GenImage image={image} loading={loading} />
         </div>
       </div>
     </>
