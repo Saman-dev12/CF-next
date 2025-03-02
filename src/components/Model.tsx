1ba14eafdeddde5af194f7366c1f31b9b7ad2b81
samanpreets84 <132365157+Saman-dev12@users.noreply.github.com>
2025-03-02 21:14:10 +0530

    Bit responsiveness

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -3,20 +3,20 @@ import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import { Input } from "./ui/input";
 import { z } from "zod";
@@ -24,8 +24,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
 const FormSchema = z.object({
-    prompt: z.string().min(1),
-    model: z.string().min(1),
+  prompt: z.string().min(1),
+  model: z.string().min(1),
 });
 interface ModelProps {
   generatedImage: (data: string) => void;
@@ -36,106 +36,106 @@ export const runtime = "edge";
 
 export const Model: React.FC<ModelProps> = ({ generatedImage,setLoadingState }) => {
 
-    // const [options, setOptions] = useState<string[]>([]);
+  // const [options, setOptions] = useState<string[]>([]);
   
-    // useEffect(() => {
-    //   const getModels = async () => {
-    //     try {
-    //       const response = await fetch("/api/models");
-    //       const data:any = await response.json();
-    //       setOptions(data.map((model: any) => model.name));
-    //     } catch (error) {
-    //       toast.error("Failed to fetch models");
-    //       console.error(error);
-    //     }
-    //   };
-    //   getModels();
-    // }, []);
+  // useEffect(() => {
+  //   const getModels = async () => {
+  //     try {
+  //       const response = await fetch("/api/models");
+  //       const data:any = await response.json();
+  //       setOptions(data.map((model: any) => model.name));
+  //     } catch (error) {
+  //       toast.error("Failed to fetch models");
+  //       console.error(error);
+  //     }
+  //   };
+  //   getModels();
+  // }, []);
   
-    const form = useForm<z.infer<typeof FormSchema>>({
-      resolver: zodResolver(FormSchema),
-      defaultValues: {
-        prompt: '',
-        model: ''
-      }
-    });
-  
-    async function onSubmit(data: z.infer<typeof FormSchema>) {
-      setLoadingState(true);
-      try {
-        
-      const response: Response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-      const blob:any = await response.blob();
-      // console.log(blob)
-      const imageUrl = URL.createObjectURL(blob);
-      console.log(imageUrl)
-
-      await generatedImage(imageUrl);
-      
-      toast.success("Image generated successfully");
-      } catch (error) {
-        toast.error("Failed to generate image");
-        console.error(error);
-      }finally {
-        setLoadingState(false);
-      }
-      
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+    prompt: '',
+    model: ''
     }
+  });
   
-    return (
-      <div className='w-full h-screen flex flex-col pt-10'>
-        <h1 className='text-2xl font-bold text-center'>Select model</h1>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <div className='flex justify-center flex-col gap-3 p-4'>
-              <FormField
-                control={form.control}
-                name="model"
-                render={({ field }) => (
-                  <FormItem>
-                    <Select 
-                      onValueChange={field.onChange} 
-                      value={field.value}
-                    >
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select model" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {/* {options.map((option, index) => (
-                          <SelectItem key={index} value={option}>{option}</SelectItem>
-                        ))} */}
-                        
-                          <SelectItem  value={"@cf/black-forest-labs/flux-1-schnell"}>{"@cf/black-forest-labs/flux-1-schnell"}</SelectItem>
-                    
-                      </SelectContent>
-                    </Select>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="prompt"
-                render={({ field }) => (
-                  <FormItem>
-                    <Input
-                      {...field}
-                      type='text'
-                      placeholder='Enter your prompt'
-                      className='p-2 border rounded mb-4'
-                    />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Generate</Button>
-            </div>
-          </form>
-        </Form>
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    setLoadingState(true);
+    try {
+    
+    const response: Response = await fetch("/api/generate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    });
+    const blob:any = await response.blob();
+    // console.log(blob)
+    const imageUrl = URL.createObjectURL(blob);
+    console.log(imageUrl)
+
+    await generatedImage(imageUrl);
+    
+    toast.success("Image generated successfully");
+    } catch (error) {
+    toast.error("Failed to generate image");
+    console.error(error);
+    }finally {
+    setLoadingState(false);
+    }
+    
+  }
+  
+  return (
+    <div className='w-full h-screen flex flex-col pt-10 px-4 md:px-10'>
+    <h1 className='text-2xl font-bold text-center mb-4'>Select model</h1>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full max-w-lg mx-auto">
+      <div className='flex flex-col gap-3'>
+        <FormField
+        control={form.control}
+        name="model"
+        render={({ field }) => (
+          <FormItem>
+          <Select 
+            onValueChange={field.onChange} 
+            value={field.value}
+          >
+            <SelectTrigger>
+            <SelectValue placeholder="Select model" />
+            </SelectTrigger>
+            <SelectContent>
+            {/* {options.map((option, index) => (
+              <SelectItem key={index} value={option}>{option}</SelectItem>
+            ))} */}
+            
+              <SelectItem  value={"@cf/black-forest-labs/flux-1-schnell"}>{"@cf/black-forest-labs/flux-1-schnell"}</SelectItem>
+          
+            </SelectContent>
+          </Select>
+          </FormItem>
+        )}
+        />
+        <FormField
+        control={form.control}
+        name="prompt"
+        render={({ field }) => (
+          <FormItem>
+          <Input
+            {...field}
+            type='text'
+            placeholder='Enter your prompt'
+            className='p-2 border rounded mb-4'
+          />
+          </FormItem>
+        )}
+        />
+        <Button type="submit" className="w-full md:w-auto">Generate</Button>
       </div>
-    );
+      </form>
+    </Form>
+    </div>
+  );
   }
